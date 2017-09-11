90d175dd68ccfc93ea82a4e50c12cbb941a1ee51
iskore <iskore@users.noreply.github.com>
2017-09-11 14:52:11 -0400

    first commit - webpack setup

---
+++ app/js/index.js
@@ -0,0 +1,7 @@
+'use strict';
+
+import 'babel-polyfill';
+
+import './global';
+
+import '../css/main.scss';
---
+++ webpack-config/webpack.dev.config.js
@@ -0,0 +1,54 @@
+'use strict';
+
+import { resolve, join } from 'path';
+import ExtractTextPlugin from 'extract-text-webpack-plugin';
+import CopyWebpackPlugin from 'copy-webpack-plugin';
+
+const
+    extractSass    = new ExtractTextPlugin( './css/main.css' ),
+    extractAssests = new CopyWebpackPlugin( [ {
+        from: './assets/',
+        to: '../dist/assets/'
+    } ] );
+
+export default {
+    context: resolve( __dirname, '../app' ),
+    entry: {
+        app: [ './js/index.js' ]
+    },
+    output: {
+        path: resolve( __dirname, '../dist' ),
+        filename: './js/bundle.js',
+        publicPath: '/'
+    },
+    module: {
+        rules: [
+            {
+                test: /\.(jpe?g|gif|png|svg|woff|ttf|wav|mp3)$/,
+                include: resolve( __dirname, '../app/assets' )
+            },
+            {
+                test: /\.scss$/,
+                include: resolve( __dirname, '../app/css' ),
+                use: extractSass.extract( {
+                    fallback: 'style-loader',
+                    use: [ 'css-loader', 'sass-loader' ]
+                } )
+            },
+            {
+                test: /\.(woff|woff2)$/,
+                use: {
+                    loader: 'file-loader?publicPath=../&name=/fonts/[name].[ext]'
+                }
+            }
+        ]
+    },
+    plugins: [
+        extractSass,
+        extractAssests
+    ],
+    devServer: {
+        compress: true,
+        port: 8080
+    }
+};
